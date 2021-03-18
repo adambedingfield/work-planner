859cf5a14ad1e05b735b416e7c7753d954df0146
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-03-17 22:34:18 -0700

    styled the time,task, save rows and tasks change color now based on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -6,65 +6,65 @@ var showDate = $(currentDay).text(displayDay + ',' + currentDate);
 var timeOfDay = [
     {
         time: "09",
-        hour: "09",
+        hour: "9",
         meridiem: "am",
-        reminder: "",
+        userInput: "",
         id: "0"
     },
     {
         time: "10",
         hour: "10",
         meridiem: "am",
-        reminder: "",
+        userInput: "",
         id: "1"
     },
     {
         time: "11",
         hour: "11",
         meridiem: "am",
-        reminder: "",
+        userInput: "",
         id: "2"
     },
     {
         time: "12",
         hour: "12",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "3"
     },
     {
         time: "13",
-        hour: "01",
+        hour: "1",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "4"
     },
     {
         time: "14",
-        hour: "02",
+        hour: "2",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "5"
     },
     {
         time: "15",
-        hour: "03",
+        hour: "3",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "6"
     },
     {
         time: "16",
-        hour: "04",
+        hour: "4",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "7"
     },
     {
         time: "17",
-        hour: "05",
+        hour: "5",
         meridiem: "pm",
-        reminder: "",
+        userInput: "",
         id: "8"
     },
     
@@ -76,22 +76,35 @@ timeOfDay.forEach(function(thisHour) {
     $(".container").append(hourRow);
 
     // creates time field
+    hourContainer = $("<div>").addClass("col-1 time-block");
     var taskHour = $("<div>").text(thisHour.hour + thisHour.meridiem);
-    taskHour.addClass('hour');
+    taskHour.addClass("hour");
+    hourContainer.append(taskHour);
 
     // task container
-    var taskContainer = $("<div>").addClass("")
+    var taskContainer = $("<div>").addClass("col-10")
     var taskInput = $("<textarea>");
+    taskInput.addClass("col-12");
+    taskInput.attr("id", this.id);
     taskContainer.append(taskInput);
 
+    if (thisHour.time < moment().format("HH")) {
+        taskInput.addClass("past");
+    } else if (thisHour.time === moment().format("HH")) {
+        taskInput.addClass("present");
+    } else if (thisHour.time > moment().format("HH")) {
+        taskInput.addClass("future");
+    }
+
     // save button
-    var saveContainer = $("<div>").addClass("");
-    var saveButton = $("<button>")
-    saveButton.addClass("oi oi-clipboard")
+    var saveContainer = $("<div>").addClass("saveBtn clipboard:hover col-1");
+    var saveButton = $("<button>");
+    saveButton.addClass("oi oi-clipboard clipboard");
     saveContainer.append(saveButton);
 
 
 
-    hourRow.append(taskHour, taskContainer, saveContainer);
+
+    hourRow.append(hourContainer, taskContainer, saveContainer);
 })
 
